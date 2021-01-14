101398780c213eea7c2e1046f556f0e7c5e05fcc
Tim Muessig <Tim.Muessig@microsoft.com>
2021-01-14 11:20:09 -0600

    Demo Features

--- TagDemo.ps1
+++ TagDemo.ps1
@@ -1,3 +1,6 @@
 # Git Version 1.1
 # Making some minor changes to see how if affect the release and tagging of stuff...
 Write-Host "Hello World, working with Tags today, which leads into RELEASES.  Yeah Baby!"
+
+
+#This is the Pre-Release stuff right here... Quiet about it please
